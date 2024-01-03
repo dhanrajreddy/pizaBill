3aa3e3bb6edeaab095537b506546ce3b96c00af7
dhanrajreddy <dhanrajkutwade7@gmail.com>
2024-01-04 01:57:48 +0530

    addedFiles

---
+++ pizzabilling/src/main/java/com/example/pizzabilling/PizzabillingApplication.java
@@ -0,0 +1,13 @@
+package com.example.pizzabilling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzabillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzabillingApplication.class, args);
+	}
+
+}
---
+++ pizzabilling/src/test/java/com/example/pizzabilling/PizzabillingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pizzabilling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzabillingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
